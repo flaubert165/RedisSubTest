143a3a500e1c904943f748a3fd957f8f50002c5c
Italo Flauber Cavalcanti de Santana <italo.santana@xpi.com.br>
2020-07-04 17:10:11 -0300

    init

---
+++ RedisSubTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisSubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var redis = RedisStore.RedisCache;
+
+                var sub = redis.Multiplexer.GetSubscriber().Subscribe(new RedisChannel("test124", RedisChannel.PatternMode.Auto));
+
+                sub.OnMessage(message => {
+                    Console.WriteLine(message.Message.ToString());
+                });
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RedisSubTest/RedisStore.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisSubTest
+{
+    public class RedisStore
+    {
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection;
+
+        static RedisStore()
+        {
+            var configurationOptions = ConfigurationOptions.Parse("localhost:7000");
+            configurationOptions.KeepAlive = 10;
+            configurationOptions.SyncTimeout = 500;
+            configurationOptions.AllowAdmin = true;
+
+            LazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(configurationOptions));
+        }
+
+        public static ConnectionMultiplexer Connection => LazyConnection.Value;
+
+        public static IDatabase RedisCache => Connection.GetDatabase();
+    }
+}
